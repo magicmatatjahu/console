554456c9feaa905a57179f34cb2e57b546a12ac4
Mateusz Puczynski <mateusz.puczynski@sap.com>
2019-01-18 10:54:13 +0100

    Change dropdown

--- components/react/src/components/Dropdown/index.js
+++ components/react/src/components/Dropdown/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import Button from '../Button';
+// import Button from '../Button';
 
 import { RelativeWrapper, DropdownWrapper } from './components';
-
+import { Dropdown as Drpd } from '../../fundamentals-react/Dropdown/Dropdown';
+import { Button } from '../../fundamentals-react/Button/Button';
+import { Popover } from '../../fundamentals-react/Popover/Popover';
+import { Menu, MenuList } from '../../fundamentals-react/Menu/Menu';
 class Dropdown extends React.Component {
   static propTypes = {
     children: PropTypes.any.isRequired,
@@ -32,13 +35,13 @@ class Dropdown extends React.Component {
     };
   }
 
-  componentWillMount() {
-    document.addEventListener('mousedown', this.handleClickOutside, false);
-  }
+  // componentWillMount() {
+  //   document.addEventListener('mousedown', this.handleClickOutside, false);
+  // }
 
-  componentWillUnmount() {
-    document.removeEventListener('mousedown', this.handleClickOutside, false);
-  }
+  // componentWillUnmount() {
+  //   document.removeEventListener('mousedown', this.handleClickOutside, false);
+  // }
 
   toggleDropdown = () => {
     this.setState({
@@ -82,37 +85,49 @@ class Dropdown extends React.Component {
       : '';
 
     return (
-      <RelativeWrapper innerRef={node => (this.node = node)}>
-        <Button
-          normal
-          width={buttonWidth}
-          padding={
-            buttonWidth
-              ? '9px 0'
-              : !primary && !secondary
-                ? '9px 0 9px 16px'
-                : '9px 16px'
+      // <RelativeWrapper innerRef={node => (this.node = node)}>
+      //   <Button
+      //     normal
+      //     width={buttonWidth}
+      //     padding={
+      //       buttonWidth
+      //         ? '9px 0'
+      //         : !primary && !secondary
+      //           ? '9px 0 9px 16px'
+      //           : '9px 16px'
+      //     }
+      //     first={firstButton}
+      //     last={lastButton}
+      //     marginTop={marginTop}
+      //     primary={primary}
+      //     secondary={secondary}
+      //     onClick={this.toggleDropdown}
+      //     disabled={!enabled}
+      //     data-e2e-id={`toggle-${itemId}`}
+      //   >
+      //     {name}
+      //   </Button>
+      //   <DropdownWrapper
+      //     visible={visible}
+      //     arrowTop={arrowTop}
+      //     arrowTopRight={arrowTopRight}
+      //     data-e2e-id={`wrapper-${itemId}`}
+      //   >
+      //     {children}
+      //   </DropdownWrapper>
+      // </RelativeWrapper>
+      <Drpd>
+        <Popover
+          noArrow
+          alignment="right"
+          control={
+            <Button dropdown disabled={!enabled}>
+              {name}
+            </Button>
           }
-          first={firstButton}
-          last={lastButton}
-          marginTop={marginTop}
-          primary={primary}
-          secondary={secondary}
-          onClick={this.toggleDropdown}
-          disabled={!enabled}
-          data-e2e-id={`toggle-${itemId}`}
-        >
-          {name}
-        </Button>
-        <DropdownWrapper
-          visible={visible}
-          arrowTop={arrowTop}
-          arrowTopRight={arrowTopRight}
-          data-e2e-id={`wrapper-${itemId}`}
-        >
-          {children}
-        </DropdownWrapper>
-      </RelativeWrapper>
+          body={children}
+        />
+      </Drpd>
     );
   }
 }
