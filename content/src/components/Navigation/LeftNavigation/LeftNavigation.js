a88ee42b8f3016c446ccf98526ae82609de8cc46
Maciej Urbańczyk <maciej.urbanczyk@sap.com>
2019-01-23 15:26:42 +0100

    Remove warnings

--- content/src/components/Navigation/LeftNavigation/LeftNavigation.js
+++ content/src/components/Navigation/LeftNavigation/LeftNavigation.js
@@ -1,20 +1,38 @@
 import React from "react";
+<<<<<<< HEAD
 import { Icon } from '@kyma-project/react-components';
 import NavigationList from "./NavigationList/NavigationList";
 
 import {
   GoBack
+=======
+import { Toolbar } from '@kyma-project/react-components';
+import NavigationList from "./NavigationList/NavigationList";
+
+import {
+  ToolbarWrapper
+>>>>>>> Remove warnings
 } from './styled';
 
 function LeftNavigation(props) {
   return (
     <>
+<<<<<<< HEAD
       <GoBack
         data-e2e-id="go-to-environment"
         onClick={props.history.goBack}
       >
         <Icon size="m" glyph="nav-back" />Back to Environment
       </GoBack>
+=======
+      <ToolbarWrapper>
+        <Toolbar
+          title="Docs"
+          addSeparator
+          goBack={props.history.goBack}
+        />
+      </ToolbarWrapper>
+>>>>>>> Remove warnings
       <NavigationList {...props} />
     </>
   );
