0cb7f692958902d060fc698ffd3b4873c3a2f5e3
Maciej Urbańczyk <maciej.urbanczyk@sap.com>
2019-01-24 13:43:45 +0100

    Change ids for tests

--- content/src/components/Navigation/LeftNavigation/LeftNavigation.js
+++ content/src/components/Navigation/LeftNavigation/LeftNavigation.js
@@ -1,38 +1,15 @@
-import React from "react";
-<<<<<<< HEAD
+import React from 'react';
 import { Icon } from '@kyma-project/react-components';
-import NavigationList from "./NavigationList/NavigationList";
+import NavigationList from './NavigationList/NavigationList';
 
-import {
-  GoBack
-=======
-import { Toolbar } from '@kyma-project/react-components';
-import NavigationList from "./NavigationList/NavigationList";
-
-import {
-  ToolbarWrapper
->>>>>>> Remove warnings
-} from './styled';
+import { GoBack } from './styled';
 
 function LeftNavigation(props) {
   return (
     <>
-<<<<<<< HEAD
-      <GoBack
-        data-e2e-id="go-to-environment"
-        onClick={props.history.goBack}
-      >
+      <GoBack data-e2e-id="go-to-environment" onClick={props.history.goBack}>
         <Icon size="m" glyph="nav-back" />Back to Environment
       </GoBack>
-=======
-      <ToolbarWrapper>
-        <Toolbar
-          title="Docs"
-          addSeparator
-          goBack={props.history.goBack}
-        />
-      </ToolbarWrapper>
->>>>>>> Remove warnings
       <NavigationList {...props} />
     </>
   );
