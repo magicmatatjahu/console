2f8e7b856c7f0db48adc2e6b9f4bfb4570665c01
Maciej Urbańczyk <maciej.urbanczyk@sap.com>
2019-01-24 09:49:26 +0100

    Improve filtering in instances

--- instances/src/components/ServiceInstances/ServiceInstancesToolbar/FilterDropdown.component.js
+++ instances/src/components/ServiceInstances/ServiceInstancesToolbar/FilterDropdown.component.js
@@ -1,43 +1,55 @@
 import React from 'react';
 
 import {
+  Button,
   Dropdown,
   FormSet,
   FormInput,
   FormItem,
 } from '@kyma-project/react-components';
 
-import { FormLabel, Panel, PanelBody } from './styled';
+import { FiltersDropdownWrapper, FormLabel, Panel, PanelBody } from './styled';
 
-const FilterDropdown = ({ filter, activeValues = [], onChange }) =>
-  !filter ? null : (
-    <Dropdown name="Filter" enabled={filter.values && filter.values.length > 0}>
-      <Panel>
-        <PanelBody>
-          <FormSet>
-            {filter.values.map((item, index) => {
-              const count = item.count !== null ? ` (${item.count})` : '';
-              // const active = activeValues.some(value => value === item.value);
+const FilterDropdown = ({ filter, activeValues = [], onChange }) => {
+  const control = (
+    <Button option="emphasized">
+      Filter
+    </Button>
+  )
 
-              return (
-                <FormItem isCheck key={index}>
-                  <FormInput
-                    type="checkbox"
-                    id={`checkbox-${index}`}
-                    name={`checkbox-name-${index}`}
-                    onClick={() => onChange(filter.name, item.value)}
-                  />
-                  <FormLabel>
-                    {item.name}
-                    {count}
-                  </FormLabel>
-                </FormItem>
-              );
-            })}
-          </FormSet>
-        </PanelBody>
-      </Panel>
-    </Dropdown>
-  );
+  return (
+    !filter ? null : (
+      <FiltersDropdownWrapper>
+        <Dropdown control={control} enabled={filter.values && filter.values.length > 0}>
+          <Panel>
+            <PanelBody>
+              <FormSet>
+                {filter.values.map((item, index) => {
+                  const count = item.count !== null ? ` (${item.count})` : '';
+                  // const active = activeValues.some(value => value === item.value);
+    
+                  return (
+                    <FormItem isCheck key={index}>
+                      <FormInput
+                        type="checkbox"
+                        id={`checkbox-${index}`}
+                        name={`checkbox-name-${index}`}
+                        onClick={() => onChange(filter.name, item.value)}
+                      />
+                      <FormLabel>
+                        {item.name}
+                        {count}
+                      </FormLabel>
+                    </FormItem>
+                  );
+                })}
+              </FormSet>
+            </PanelBody>
+          </Panel>
+        </Dropdown>
+      </FiltersDropdownWrapper>
+    )
+  )
+}
 
 export default FilterDropdown;
