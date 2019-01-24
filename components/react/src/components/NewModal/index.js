02786d00a7f1e504e5aacc6e46ff027071a394f6
Mateusz Puczynski <mateusz.puczynski@sap.com>
2019-01-24 09:52:06 +0100

    Add first changes for CreateInstanceModal

--- components/react/src/components/NewModal/index.js
+++ components/react/src/components/NewModal/index.js
@@ -5,96 +5,124 @@ import Button from '../Button';
 import { FdModal } from './styled';
 
 class Modal extends Component {
-    state = {
-        show: false,
+  state = {
+    show: false,
+  };
+
+  static propTypes = {
+    title: PropTypes.any,
+    modalOpeningComponent: PropTypes.any.isRequired,
+    actions: PropTypes.any,
+    onShow: PropTypes.func,
+    onHide: PropTypes.func,
+    onConfirm: PropTypes.func.isRequired,
+    confirmText: PropTypes.string,
+    cancelText: PropTypes.string,
+    type: PropTypes.string,
+    disabledConfirm: PropTypes.bool,
+    waiting: PropTypes.bool,
+  };
+
+  static defaultProps = {
+    title: 'Modal',
+    confirmText: 'Confirm',
+    cancelText: 'Cancel',
+    actions: null,
+    type: 'default',
+    disabledConfirm: false,
+    waiting: false,
+  };
+
+  onOpen = () => {
+    const { onShow } = this.props;
+    if (onShow && typeof onShow === 'function') {
+      onShow();
+    }
+    this.setState({ show: true });
+  };
+
+  onClose = () => {
+    const { onHide } = this.props;
+    if (onHide && typeof onHide === 'function') {
+      onHide();
+    }
+    this.setState({ show: false });
+  };
+
+  onConfirmation = () => {
+    const { onConfirm } = this.props;
+    if (onConfirm && typeof onConfirm === 'function') {
+      onConfirm();
+    }
+    this.onClose();
+  };
+
+  confirmActions = () => {
+    const {
+      props: { confirmText, cancelText, type, disabledConfirm, waiting },
+    } = this;
+
+    const confirmMessage = waiting ? (
+      <div style={{ width: '97px', height: '16px' }}>
+        <Spinner color="#fff" padding="0 16px" size="14px" />
+      </div>
+    ) : (
+      confirmText
+    );
+
+    return (
+      <Fragment>
+        <Button
+          style={{ marginRight: '12px' }}
+          option="light"
+          onClick={this.onClose}
+        >
+          {cancelText}
+        </Button>
+        <Button
+          type={type}
+          onClick={this.onConfirmation}
+          disabled={disabledConfirm}
+        >
+          {confirmMessage}
+        </Button>
+      </Fragment>
+    );
+  };
+
+  render() {
+    const {
+      props: {
+        children,
+        title,
+        modalOpeningComponent,
+        actions,
+        onConfirm,
+        type,
+      },
+      state: { show },
+    } = this;
+
+    let ac = actions;
+    if (!ac && onConfirm && typeof onConfirm === 'function') {
+      ac = this.confirmActions();
     }
 
-    static propTypes = {
-        title: PropTypes.any,
-        modalOpeningComponent: PropTypes.any.isRequired,
-        actions: PropTypes.any,
-        onShow: PropTypes.func,
-        onHide: PropTypes.func,
-        onConfirm: PropTypes.func.isRequired,
-        confirmText: PropTypes.string,
-        cancelText: PropTypes.string,
-        type: PropTypes.string,
-        disabledConfirm: PropTypes.bool,
-    };
-
-    static defaultProps = {
-        title: 'Modal',
-        confirmText: "Confirm",
-        cancelText: "Cancel",
-        actions: null,
-        type: "default",
-        disabledConfirm: false,
-    };
-
-    onOpen = () => {
-        const { onShow } = this.props;
-        if (onShow && typeof onShow === 'function') {
-          onShow();
-        }
-        this.setState({ show: true })
-    }
-
-    onClose = () => {
-        const { onHide } = this.props;
-        if (onHide && typeof onHide === 'function') {
-          onHide();
-        }
-        this.setState({ show: false })
-    }
-
-    onConfirmation = () => {
-        const { onConfirm } = this.props;
-        if (onConfirm && typeof onConfirm === 'function') {
-          onConfirm();
-        }
-        this.onClose();
-    }
-
-    confirmActions = () => {
-        const { props: { confirmText, cancelText, type, disabledConfirm } } = this;
-
-        return (
-            <Fragment>
-                <Button
-                    style={{ marginRight: "12px" }}
-                    option="light"
-                    onClick={this.onClose}
-                >
-                    {cancelText}
-                </Button>
-                <Button
-                    type={type}
-                    onClick={this.onConfirmation}
-                    disabled={disabledConfirm}
-                >
-                    {confirmText}
-                </Button>
-            </Fragment>
-        )
-    }
-
-    render() {
-        const { props: { children, title, modalOpeningComponent, actions, onConfirm, type }, state: { show } } = this;
-
-        let ac = actions;
-        if (!ac && onConfirm && typeof onConfirm === 'function') {
-            ac = this.confirmActions();
-        }
-
-        return (
-            <Fragment>
-                <div onClick={this.onOpen}>{modalOpeningComponent}</div>
-                <FdModal type={type} title={title} show={show} onClose={this.onClose} actions={ac}>
-                    {children}
-                </FdModal>
-            </Fragment>
-        );
-    }
+    return (
+      <Fragment>
+        <div onClick={this.onOpen}>{modalOpeningComponent}</div>
+        <FdModal
+          type={type}
+          title={title}
+          show={show}
+          onClose={this.onClose}
+          actions={ac}
+        >
+          {children}
+        </FdModal>
+      </Fragment>
+    );
+  }
 }
 
 export default Modal;
