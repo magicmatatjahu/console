04d810c67238dc3c86fbd17612c7c7ddc3d212aa
Mateusz Puczynski <mateusz.puczynski@sap.com>
2019-01-25 09:16:56 +0100

    Add tooltip to NewModal and fix onSubmit w/ button

--- catalog/src/components/ServiceClassDetails/CreateInstanceModal/CreateInstanceModal.component.js
+++ catalog/src/components/ServiceClassDetails/CreateInstanceModal/CreateInstanceModal.component.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import {
-  Separator,
-  Modal,
-} from '@kyma-project/react-components';
+import { Separator, Modal } from '@kyma-project/react-components';
 import LuigiClient from '@kyma-project/luigi-client';
 
 import BasicData from './BasicData.component';
@@ -192,8 +189,8 @@ class CreateInstanceModal extends React.Component {
     const {
       formData,
       firstStepFilled,
-      // tooltipData,
       creatingInstance,
+      tooltipData,
       instanceCreateParameters,
     } = this.state;
 
@@ -243,8 +240,7 @@ class CreateInstanceModal extends React.Component {
               onSubmitSchemaForm={this.onSubmitSchemaForm}
               planName={schema.displayName}
               callback={this.callback}
-            >
-            </SchemaData>
+            />
           </div>
         )}
       </div>
@@ -255,21 +251,18 @@ class CreateInstanceModal extends React.Component {
         width={'681px'}
         title={
           <p style={{ marginRight: '25px' }}>
-           Provision the{' '}
-           <Bold>{serviceClass.displayName}</Bold>{' '}
-           {serviceClass.__typename === 'ClusterServiceClass'
-             ? 'Cluster Service Class'
-             : 'Service Class'}{' '}
-           in the{' '}
-           <Bold>{environment}</Bold>
-           {' '}Namespace
-         </p>
+            Provision the <Bold>{serviceClass.displayName}</Bold>{' '}
+            {serviceClass.__typename === 'ClusterServiceClass'
+              ? 'Cluster Service Class'
+              : 'Service Class'}{' '}
+            in the <Bold>{environment}</Bold> Namespace
+          </p>
         }
         type={'emphasized'}
         modalOpeningComponent={modalOpeningComponent}
         confirmText="Create Instance"
         cancelText="Cancel"
-        //tooltipData={tooltipData}
+        tooltipData={tooltipData}
         disabledConfirm={disabled}
         onConfirm={this.handleConfirmation}
         handleClose={this.clearState}
